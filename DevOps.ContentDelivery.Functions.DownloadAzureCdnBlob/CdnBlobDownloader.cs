4ee58aa5c341ce722bb07fd29131c751b81b7c32
Christopher Dorst <chris@dorst.email>
2017-12-30 22:48:23 -0600

    Add CdnBlobDownloader

---
+++ DevOps.ContentDelivery.Functions.DownloadAzureCdnBlob/CdnBlobDownloader.cs
@@ -0,0 +1,14 @@
+﻿using DevOps.ContentDelivery.Structures.AzureBlob;
+using static Common.Functions.DownloadFile.FileDownloader;
+using static DevOps.ContentDelivery.Functions.ResolveAzureCdnBlobUrl.UrlResolver;
+
+namespace DevOps.ContentDelivery.Functions.DownloadAzureCdnBlob
+{
+    public static class CdnBlobDownloader
+    {
+        public static void DownloadBlob(string endpointName, Blob blob, string destinationPath)
+            => Download(
+                Uri(endpointName, blob),
+                destinationPath);
+    }
+}
